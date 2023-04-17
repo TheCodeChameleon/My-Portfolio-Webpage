968714fd752e18e6da7fb64a232e20e4f50d69f7
The Code Chameleon <thecodechameleon@gmail.com>
2023-04-17 20:30:13 -0300

    TypeWriter ADDED

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+const fullName = document.querySelector(".main-title");
+const nameComplete = fullName.innerHTML;
+
+function TypeWriter(name) {
+  setTimeout(() => {
+    const myName = name.split("");
+    fullName.innerHTML = "";
+    myName.forEach((letter, index) => {
+      setTimeout(function () {
+        fullName.innerHTML += letter;
+      }, 150 * index);
+    });
+  }, 1500);
+}
+
 document.getElementById("botao-copiar").addEventListener("click", function () {
-  var textoCopiado = document.getElementById("texto-copiado");
-  var areaDeTransferencia = document.createElement("textarea");
-  areaDeTransferencia.value = textoCopiado.textContent;
-  document.body.appendChild(areaDeTransferencia);
-  areaDeTransferencia.select();
+  var copyText = document.getElementById("texto-copiado");
+  var transferArea = document.createElement("textarea");
+  transferArea.value = copyText.textContent;
+  document.body.appendChild(transferArea);
+  transferArea.select();
   document.execCommand("copy");
-  document.body.removeChild(areaDeTransferencia);
+  document.body.removeChild(transferArea);
 });
+
+TypeWriter(nameComplete);
