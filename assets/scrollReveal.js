eed863b0f0646bce8d3c0abeeccf947de70037a7
TheCodeChameleon <thecodechameleon@gmail.com>
2023-04-27 21:08:53 -0300

    LESS DURATION

--- assets/scrollReveal.js
+++ assets/scrollReveal.js
@@ -2,10 +2,10 @@ window.sr = ScrollReveal({ reset: true });
 
 ScrollReveal({ distance: "15px" });
 sr.reveal(".scroll-1", {
-  duration: 2500,
+  duration: 1000,
   origin: "bottom",
 });
 sr.reveal(".scroll-2", {
-  duration: 2500,
+  duration: 2000,
   origin: "bottom",
 });
